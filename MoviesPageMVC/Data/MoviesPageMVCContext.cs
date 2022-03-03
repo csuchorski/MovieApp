9d6ac713c7f1cf676033748ec162a542be966d01
Cezary Suchorski <cezary.suchorski@wp.pl>
2022-03-03 21:40:13 +0100

    added movie model and migrated into sql server

---
+++ MoviesPageMVC/Data/MoviesPageMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MoviesPageMVC.Models;
+
+namespace MoviesPageMVC.Data
+{
+    public class MoviesPageMVCContext : DbContext
+    {
+        public MoviesPageMVCContext (DbContextOptions<MoviesPageMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MoviesPageMVC.Models.Movie> Movie { get; set; }
+    }
+}
