ec31f269bd32420d0d54509ecaf888c50adcfb26
Cezary Suchorski <cezary.suchorski@wp.pl>
2022-03-07 19:56:37 +0100

    Added search functionality to Index

--- MoviesPageMVC/Models/Movie.cs
+++ MoviesPageMVC/Models/Movie.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,10 +11,11 @@ namespace MoviesPageMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
     }
 }
